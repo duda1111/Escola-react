425991fa31808bca416bb49397d21ca0ce6da2c0
duda1111 <eduarda.freitas.pereira@escola.pr.gov.br>
2025-10-17 10:03:14 -0300

    alteraçao

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function footer(){
+  return(
+      <footer className="Footer">
+        <div className="Footer-bottom" >Otalipio Pereira de Andrade</div>
+      </footer>
+  );
+}
